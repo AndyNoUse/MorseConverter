71b66ea78980a2b85b1e2a4e983e24a168146bee
AndyNoUse <62490458+AndyNoUse@users.noreply.github.com>
2025-02-07 21:16:08 +0100

    add convertTo... Tests and added function load loadMorseToLetterlib, convertMorse, getOutput

---
+++ src/MorseTest.java
@@ -0,0 +1,80 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MorseTest {
+    /*
+    Test-klass Testa logikklassen med 5 testfall. Tips ENKLA saker, en bokstav, en morsekod
+flera tecken, olika tecken. Testa felhanteringar i systemet. Stoppar vi in något som
+inte ska vara i systemet så ska vi KANSKE få tomt svar eller felmeddelande
+A .-
+B -...
+C -.-.
+D -..
+E .
+F ..-.
+G --.
+H ....
+I ..
+J .---
+K -.-
+L .-..
+M --
+N -.
+O ---
+P .--.
+Q --.-
+R .-.
+S ...
+T -
+U ..-
+V ...-
+W .--
+X -..-
+Y -.--
+Z --..
+----- 0 -----
+.---- 1 .----
+..--- 2 ..---
+...-- 3 ...--
+....- 4 ....-
+..... 5 .....
+-.... 6 -....
+--... 7 --...
+---.. 8 ---..
+----. 9 ----.
+     */
+    @Test
+    public void convertToA() {
+        Logic converter = new Logic();
+        converter.loadMorseToLetterLib();
+        converter.convertMorse(".-");
+        converter.getOutput();
+        assertEquals("A", converter.getOutput());
+    }
+    @Test
+    public void convertToC() {
+        Logic converter = new Logic();
+        converter.loadMorseToLetterLib();
+        converter.convertMorse("-.-.");
+        converter.getOutput();
+        assertEquals("C", converter.getOutput());
+    }
+    @Test
+    public void convertTo9() {
+        Logic converter = new Logic();
+        converter.loadMorseToLetterLib();
+        converter.convertMorse("----.");
+        converter.getOutput();
+        assertEquals("9", converter.getOutput());
+    }
+    @Test
+    public void convertToZ() {
+        Logic converter = new Logic();
+        converter.loadMorseToLetterLib();
+        converter.convertMorse("--..");
+        converter.getOutput();
+        assertEquals("Z", converter.getOutput());
+    }
+}
+
