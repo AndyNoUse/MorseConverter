6428470f223d0f1b99488c6eed57c454302cf7db
AndyNoUse <62490458+AndyNoUse@users.noreply.github.com>
2025-02-07 20:15:07 +0100

    add Simpsons template from lessons for rework into useable code.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     /*
    Krav:
@@ -42,6 +45,26 @@ Lämna in som en länk i "Ominway" med er textfil
 
     */
     public static void main(String[] args) {
+            HashMap<String, String> simpsons = new HashMap<>(); //Kan skriva String, String i den nya diamanten
+//Gör inte det för att vi redan fattar det???
+            Scanner scan = new Scanner(System.in);
 
+            simpsons.put("Homer", "D'oh");
+            simpsons.put("Marge", "Mmm~mmmmmm");
+            simpsons.put("Bart", "Ay, caramba");
+            simpsons.put("Lisa", "If anyone wants me, I'll be in my room.");
+            simpsons.put("Maggie", "(Pacifier Suck)");
+            simpsons.put("Ned", "Hi-Diddily-Ho!");
+            simpsons.put("Burns", "Excellent!");
+            simpsons.put("Edna", "Ha!");
+            simpsons.put("Chalmers", "SEEEEEYYYYMOOUURRR");
+            simpsons.put("Apu", "Thank You, Come Again!");
+            simpsons.put("Stu", "Disco Stu likes disco music");
+
+            //       System.out.println(simpsons.get("Ned"));;
+            for (int i = 0; i < 5; i++) {
+                String name = scan.nextLine();
+                System.out.println(simpsons.get(name));
+            }
+        }
     }
-}
