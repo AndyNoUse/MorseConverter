ae769a8c5bc6c9f6166d0921738f82744384f3ec
AndyNoUse <62490458+AndyNoUse@users.noreply.github.com>
2025-02-07 21:16:03 +0100

    add convertTo... Tests and added function load loadMorseToLetterlib, convertMorse, getOutput

--- src/Logic.java
+++ src/Logic.java
@@ -1,3 +1,75 @@
+import java.util.HashMap;
+
 public class Logic {
-   //Logik-klass Utför konverteringen
+    //Logik-klass Utför konverteringen
+    private String input = "";
+    private String output = "";
+    private HashMap<String, String> morseLib = new HashMap<>();
+
+    public void loadMorseToLetterLib(){
+        morseLib.put(".-", "A");
+        morseLib.put("-...", "B");
+        morseLib.put("-.-.", "C");
+        morseLib.put("-..", "D");
+        morseLib.put(".", "E");
+        morseLib.put("..-.", "F");
+        morseLib.put("--.", "G");
+        morseLib.put("....", "H");
+        morseLib.put("..", "I");
+        morseLib.put(".---", "J");
+        morseLib.put("--", "M");
+        morseLib.put("-.", "N");
+        morseLib.put("---", "O");
+        morseLib.put(".--.", "P");
+        morseLib.put("--.-", "Q");
+        morseLib.put(".-.", "R");
+        morseLib.put("...", "S");
+        morseLib.put("-", "T");
+        morseLib.put("..-", "U");
+        morseLib.put("...-", "V");
+        morseLib.put(".--", "W");
+        morseLib.put("-..-", "X");
+        morseLib.put("-.--", "X");
+        morseLib.put("--..", "Z");
+        morseLib.put("-----", "0");
+        morseLib.put(".----", "1");
+        morseLib.put("..---", "2");
+        morseLib.put("...--", "3");
+        morseLib.put("....-", "4");
+        morseLib.put(".....", "5");
+        morseLib.put("-....", "6");
+        morseLib.put("--...", "7");
+        morseLib.put("---..", "8");
+        morseLib.put("----.", "9");
+
+
+
+    }
+    public void convertMorse(String input) {
+        this.input =input;
+        output = morseLib.get(input);
+    }
+
+    public String getOutput() {
+        return output;
+    }
 }
+//public static void main(String[] args) {
+//            HashMap<String, String> simpsons = new HashMap<>(); //Kan skriva String, String i den nya diamanten
+////Gör inte det för att vi redan fattar det???
+//            Scanner scan = new Scanner(System.in);
+//
+//            simpsons.put("Homer", "D'oh");
+//            simpsons.put("Marge", "Mmm~mmmmmm");
+//            simpsons.put("Bart", "Ay, caramba");
+//            simpsons.put("Lisa", "If anyone wants me, I'll be in my room.");
+//            simpsons.put("Maggie", "(Pacifier Suck)");
+//            simpsons.put("Ned", "Hi-Diddily-Ho!");
+//            simpsons.put("Burns", "Excellent!");
+//            simpsons.put("Edna", "Ha!");
+//            simpsons.put("Chalmers", "SEEEEEYYYYMOOUURRR");
+//            simpsons.put("Apu", "Thank You, Come Again!");
+//            simpsons.put("Stu", "Disco Stu likes disco music");
+//            for (int i = 0; i < 5; i++) {
+//                String name = scan.nextLine();
+//                System.out.println(simpsons.get(name));
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
+//Läser in - Skickar till Logic - skriver ut text Klassen
 public class Main {
+    public static void main(String[] args) {
+
+    }
     /*
    Krav:
 Skriv ett program som gör om morsekod till engelska och engelska till morsekod
@@ -10,7 +14,7 @@ text (med en main-metod).
 Utveckla programmet i TDD och skriv minst 5 JUnit-testfall
 
 *Logik-klass Utför konverteringen
-Main-klass läser in text - skickar till logik - Hämtar resultatet från logik
+**Main-klass läser in text - skickar till logik - Hämtar resultatet från logik
 Test-klass Testa logikklassen med 5 testfall. Tips ENKLA saker, en bokstav, en morsekod
 flera tecken, olika tecken. Testa felhanteringar i systemet. Stoppar vi in något som
 inte ska vara i systemet så ska vi KANSKE få tomt svar eller felmeddelande
@@ -44,27 +48,4 @@ morsekoden
 Lämna in som en länk i "Ominway" med er textfil
 
     */
-    public static void main(String[] args) {
-            HashMap<String, String> simpsons = new HashMap<>(); //Kan skriva String, String i den nya diamanten
-//Gör inte det för att vi redan fattar det???
-            Scanner scan = new Scanner(System.in);
-
-            simpsons.put("Homer", "D'oh");
-            simpsons.put("Marge", "Mmm~mmmmmm");
-            simpsons.put("Bart", "Ay, caramba");
-            simpsons.put("Lisa", "If anyone wants me, I'll be in my room.");
-            simpsons.put("Maggie", "(Pacifier Suck)");
-            simpsons.put("Ned", "Hi-Diddily-Ho!");
-            simpsons.put("Burns", "Excellent!");
-            simpsons.put("Edna", "Ha!");
-            simpsons.put("Chalmers", "SEEEEEYYYYMOOUURRR");
-            simpsons.put("Apu", "Thank You, Come Again!");
-            simpsons.put("Stu", "Disco Stu likes disco music");
-
-            //       System.out.println(simpsons.get("Ned"));;
-            for (int i = 0; i < 5; i++) {
-                String name = scan.nextLine();
-                System.out.println(simpsons.get(name));
-            }
-        }
-    }
+}
